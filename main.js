d186fb0c7142ea4e699d8b3b686762fc9bb78d19
tahirkhan-01 <161066436+tahirkhan-01@users.noreply.github.com>
2024-02-23 23:31:23 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function shuffle(){
+    const img = document.getElementById('img');
+    let random = Math.floor(Math.random() * 6) + 1;
+    img.setAttribute("src", `images/${random}.png`);
+}
+
+function anim(){
+    setTimeout(shuffle, 500);
+    const img = document.getElementById('img');
+    img.setAttribute("src", `images/dice-rol.gif`);
+}
